af623b92e01d548ae80b5437eff629b1ab3e658b
TheTaz25 <thetazlp@hotmail.de>
2020-02-24 15:32:33 +0100

    Added basic statics/methods for user schema, updated schema

--- schemas/User.js
+++ schemas/User.js
@@ -1,22 +1,52 @@
 const mongoose = require('mongoose')
-const { Schema } = mongoose
+const sha = require('sha.js')
+const {
+  Schema
+} = mongoose
 
 const userSchema = new Schema({
   username: String,
   password: String,
-  role: String
+  token: String,
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
 })
 
-userSchema.statics.doesUserAlreadyExist = function(username) {
-  return this.findOne({ username })
-  .then((response) => {
-    if(!response)
-      return false
-    return true
+userSchema.statics.doesUsernameAlreadyExist = function(username, callback) {
+  return this.findOne({
+      username
+    })
+    .then((response) => {
+      if (!response) {
+        callback(false)
+      } else {
+        callback(true)
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+      callback(undefined, new Error('Could not fetch database'));
+    })
+}
+
+userSchema.methods.renewToken = function(secret, callback) {
+  const now = new Date();
+  const newToken = sha('sha256')
+    .update(now.toISOString() + this.username + secret)
+    .digest('hex')
+
+  this.token = newToken
+  this.save(err => {
+    callback(!err, newToken)
   })
-  .catch((err) => {
-    console.error(err)
-    return true
+}
+
+userSchema.methods.clearToken = function(callback) {
+  this.token = ''
+  this.save(err => {
+    callback(!err)
   })
 }
 
