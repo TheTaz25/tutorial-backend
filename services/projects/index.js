d635bc1284d73262c9edf472b2f2dbc04ea53674
TheTaz25 <thetazlp@hotmail.de>
2020-02-24 16:39:24 +0100

    Added empty routes for projects and tasks

---
+++ services/projects/index.js
@@ -0,0 +1,33 @@
+module.exports = function(fastify, opts, next) {
+  // Create a new project
+  fastify.post('/api/projects', (req, res) => {
+    res.send()
+  })
+
+  // Get a list of all/filtered projects
+  fastify.get('/api/projects', (req, res) => {
+    res.send()
+  })
+
+  // Get a specific project
+  fastify.get('/api/projects/:projectId', (req, res) => {
+    res.send()
+  })
+
+  // Get all tasks in a project
+  fastify.get('/api/projects/:projectId/tasks', (req, res) => {
+    res.send()
+  })
+
+  // Set Information on a project
+  fastify.put('/api/projects/:projectId', (req, res) => {
+    res.send()
+  })
+
+  // Delete a project forever
+  fastify.delete('/api/projects/:projectId', (req, res) => {
+    res.send()
+  })
+
+  next()
+}
