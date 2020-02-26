1dfb7d30efca1a2adf455179319863da9d4baea5
TheTaz25 <thetazlp@hotmail.de>
2020-02-26 16:25:59 +0100

    Refactored project and task routes to provide options

--- services/projects/index.js
+++ services/projects/index.js
@@ -1,31 +1,35 @@
 module.exports = function(fastify, opts, next) {
   // Create a new project
-  fastify.post('/api/projects', (req, res) => {
+  fastify.post('/api/projects', {
+    schema: {
+      body: fastify.projectCreation
+    }
+  }, (req, res) => {
     res.send()
   })
 
   // Get a list of all/filtered projects
-  fastify.get('/api/projects', (req, res) => {
+  fastify.get('/api/projects', {}, (req, res) => {
     res.send()
   })
 
   // Get a specific project
-  fastify.get('/api/projects/:projectId', (req, res) => {
+  fastify.get('/api/projects/:projectId', {}, (req, res) => {
     res.send()
   })
 
   // Get all tasks in a project
-  fastify.get('/api/projects/:projectId/tasks', (req, res) => {
+  fastify.get('/api/projects/:projectId/tasks', {}, (req, res) => {
     res.send()
   })
 
   // Set Information on a project
-  fastify.put('/api/projects/:projectId', (req, res) => {
+  fastify.put('/api/projects/:projectId', {}, (req, res) => {
     res.send()
   })
 
   // Delete a project forever
-  fastify.delete('/api/projects/:projectId', (req, res) => {
+  fastify.delete('/api/projects/:projectId', {}, (req, res) => {
     res.send()
   })
 
--- services/tasks/index.js
+++ services/tasks/index.js
@@ -1,31 +1,31 @@
 module.exports = function(fastify, opts, next) {
   // Create a new task and assign it to a project
-  fastify.post('/api/tasks', (req, res) => {
+  fastify.post('/api/tasks', {}, (req, res) => {
     res.send()
   })
 
   // Get all tasks assigned to a person
-  fastify.get('/api/tasks/:userId', (req, res) => {
+  fastify.get('/api/tasks/:userId', {}, (req, res) => {
     res.send()
   })
 
   // Update Task
-  fastify.put('/api/tasks/:taskId', (req, res) => {
-    res.send()
-  })
-
-  // Delete Task
-  fastify.delete('/api/tasks/:taskId', (req, res) => {
+  fastify.put('/api/tasks/:taskId', {}, (req, res) => {
     res.send()
   })
 
   // Add dependent to task (and notify user)
-  fastify.put('/api/tasks/:ownTaskId/dependant/:dependantTaskId', (req, res) => {
+  fastify.put('/api/tasks/:ownTaskId/dependant/:dependantTaskId', {}, (req, res) => {
     res.send()
   })
 
   // Change state of task
-  fastify.put('/api/tasks/:taskId/state/:state', (req, res) => {
+  fastify.put('/api/tasks/:taskId/state/:state', {}, (req, res) => {
+    res.send()
+  })
+
+  // Delete Task
+  fastify.delete('/api/tasks/:taskId', {}, (req, res) => {
     res.send()
   })
 
