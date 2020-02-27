4a5b0ed4eb3b53ebfe411de7fcb98ea5d75fad5a
TheTaz25 <thetazlp@hotmail.de>
2020-02-27 16:24:16 +0100

    first meaningful implementation of project route (creation)

--- services/projects/index.js
+++ services/projects/index.js
@@ -3,13 +3,70 @@ module.exports = function(fastify, opts, next) {
   fastify.post('/api/projects', {
     schema: {
       body: fastify.projectCreation
-    }
+    },
+    preHandler: [
+      fastify.isUserLoggedIn
+    ]
   }, (req, res) => {
-    res.send()
+    // Destruct body
+    const {
+      members,
+      title,
+      description
+    } = req.body
+
+    // Create new member (leader of project)
+    const projectLeader = new fastify.members({
+      memberId: members[0],
+      role: fastify.USERROLES.PROJECTLEADER
+    })
+
+    // Save ProjectLeader
+    projectLeader.save(err => {
+      if (err) {
+        res.code(500)
+          .send('Internal Server Error (Could not create new Project-Leader in database)')
+        return
+      }
+
+      // Create new Project
+      const newProject = new fasitfy.projects({
+        title,
+        description: description || '',
+        members: [projectLeader._id],
+        lastmodified: Date.now()
+      })
+
+      // Save new project
+      newProject.save(err => {
+        if (err) {
+          // In case of an unsuccessfull project save, delete the created projectleader
+          res.code(500)
+            .send('Internal Server Error (Could not create new Project in database)')
+          fastify.members.deleteOne({
+              _id: projectLeader._id
+            })
+            .then(err => {
+              fastify.log.error('Could not delete projectLeader after unsuccessfull project creation with id' + projectLeader._id)
+            })
+          return
+        }
+        res.code(201)
+          .send()
+      })
+    })
   })
 
   // Get a list of all/filtered projects
-  fastify.get('/api/projects', {}, (req, res) => {
+  fastify.get('/api/projects', {
+    preHandler: [
+      fastify.isUserLoggedIn
+    ]
+  }, (req, res) => {
+    const {
+      query
+    } = req
+
     res.send()
   })
 
