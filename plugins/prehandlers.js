a7067417b47a3fdb2d7107816199bdb10cf029fa
TheTaz25 <thetazlp@hotmail.de>
2020-03-03 14:27:27 +0100

    Added prehandler for project-routes, lean check, added comments

--- plugins/prehandlers.js
+++ plugins/prehandlers.js
@@ -2,6 +2,7 @@ const fp = require('fastify-plugin')
 
 module.exports = fp(function(fastify, opts, next) {
 
+  // Checks if the user is logged in and appends the usermodel to the request object
   fastify.decorate('isUserLoggedIn', (req, res, done) => {
     const token = req.cookies.sessionToken
     if (!token) {
@@ -12,6 +13,7 @@ module.exports = fp(function(fastify, opts, next) {
     fastify.users.findOne({
         token: req.cookies.sessionToken
       })
+      .lean(req.raw.method === "GET")
       .then(user => {
         if (!user) {
           res.code(401)
@@ -20,6 +22,36 @@ module.exports = fp(function(fastify, opts, next) {
           req.user = user
           done()
         }
+      })
+      .catch(err => {
+        res.code(500)
+        done(new Error('Failed to fetch user'))
+      })
+  })
+
+  // Check if the user is part of the project (or simply an admin) and append the project object to the request
+  fastify.decorate('isUserPartOfProject', (req, res, done) => {
+    fastify.projects.findOne({
+        _id: req.params.projectId
+      })
+      .lean(req.raw.method === "GET")
+      .then(project => {
+        if (!project) {
+          res.code(404)
+          done(new Error('Could not find project'))
+        } else {
+          if (req.user.isAdmin || project.members.includes(req.user._id)) {
+            req.project = project
+            done()
+          } else {
+            res.code(403)
+            done(new Error('User is not part of this project'))
+          }
+        }
+      })
+      .catch(err => {
+        res.code(500)
+        done(new Error('Failed to fetch project'))
       })
   })
 
