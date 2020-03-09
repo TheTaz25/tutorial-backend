3c824726774c8e4f48f8a6ab8ea2dac0a1314ef6
TheTaz25 <thetazlp@hotmail.de>
2020-03-09 15:45:53 +0100

    Further Implementation (DELETE method is not ready yet)

--- services/projects/index.js
+++ services/projects/index.js
@@ -143,31 +143,127 @@ module.exports = function(fastify, opts, next) {
       NEW TASKS -> DEVS (also: not here in this route)
       NO PUT FOR USERS
     */
-   // Check if role is Project-Leader
-   const { USERROLES } = fastify
-   const { project, body } = req
-   if(!(req.roles === USERROLES.PROJECTLEADER)) {
-     res.code(403).send('Only Projectleader of this project may change project information')
-     return
-   }
-   // Adjust
-   
-   // Update timestamp
-   project.lastmodified = Date.now()
-   // Save
-   project.save(err => {
+    // Check if role is Project-Leader
+    const {
+      USERROLES
+    } = fastify
 
-     // Done
-     res.send()
-   })
+    const {
+      project,
+      body
+    } = req
+
+    if (!(req.role === USERROLES.PROJECTLEADER)) {
+      res.code(403)
+        .send('Only Projectleader of this project may change project information')
+      return
+    }
+    // Adjust
+    if (body.hasOwnProperty('title')) {
+      project.title = body.title
+    }
+
+    if (body.hasOwnProperty('description')) {
+      project.description = body.description
+    }
+
+    if (body.hasOwnProperty('completed')) {
+      project.completed = body.completed
+    }
+
+    const errors = new Set()
+    if (body.hasOwnProperty('members')) {
+      for (const member in body.members) {
+        // Due to the schema validation, we can be sure that it is either delete or add action
+        if (member.action === 'DELETE') {
+          project.members = project.members
+            .filter(projectMember => !(projectMember._id === member.memberid))
+        } else { // ADD
+          // Role is also taken care of via the schema validation (user, projectleader or developer)
+          const newMember = new fastify.members({
+            memberId: member.memberid,
+            role: member.role
+          })
+
+          newMember.save(err => {
+            if (err) {
+              errors.add('Could not create projectmember with role ' + member.role)
+            } else {
+              project.members.push(newMember._id)
+            }
+          })
+        }
+      }
+    }
+    if (errors.size) {
+      for (const err of errors) {
+        req.log.warn(err)
+      }
+      res.code(500)
+        .send('Could not create and assign all new members to this project, please check and try again')
+      return
+    }
+
+    // Update timestamp
+    project.lastmodified = Date.now()
+    // Save
+    project.save(err => {
+      if (err) {
+        req.log.error(err)
+        res.code(500)
+          .send("Could not update project information, please try again")
+        return
+      } else {
+        // Done
+        res.send()
+      }
+    })
   })
 
   // Delete a project forever
-  fastify.delete('/api/projects/:projectId', {}, (req, res) => {
+  fastify.delete('/api/projects/:projectId', {
+    preHandler: [
+      fastify.isUserLoggedIn,
+      fastify.isUserPartOfProject,
+      fastify.assignUserRole
+    ],
+    schema: {
+      params: fastify.needsProjectId
+    }
+  }, (req, res) => {
     /*
       CRUD ALL -> PROJECT-LEADER-ONLY
     */
-    res.send()
+    const {
+      USERROLES
+    } = fastify
+
+    if (!(req.role === USERROLES.PROJECTLEADER)) {
+      req.log.warn(`User ${req.user._id} is of role ${req.role} and therefore not allowed to delete the project`)
+      res.code(403)
+        .send('Action not allowed')
+    }
+
+    // TODO: Delete members and tasks...
+    // Remember: We are working with a not-lean variant of the ressource (is a mongoose object more or
+    // less) due to delete method
+    req.project
+      .depopulate('members')
+      .depopulate('tasks')
+
+    delete req.project
+
+    fastify.projects.deleteOne({
+      _id: req.params.projectId
+    }, function(err) {
+      if (err) {
+        req.log.error('Could not delete project with id ', +req.params.projectId)
+        res.code(500)
+          .send("Could not delete project")
+      } else {
+        res.send()
+      }
+    })
   })
 
   next()
